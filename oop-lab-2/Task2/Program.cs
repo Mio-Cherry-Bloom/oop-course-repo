34c7b7bdea9bedd9d7a6fa363e210dd0f10babc4
Mio <savaluchian1@gmail.com>
2024-11-04 20:56:28 +0200

    implemented simple program class and barista class for testing

--- oop-lab-2/Task2/Program.cs
+++ oop-lab-2/Task2/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+    public class Barista
+    {
+        public void PrepareCoffee(Coffee coffee)
+        {
+            coffee.PrintDetails();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Americano americano = new Americano { CoffeeIntensity = Intensity.NORMAL, MlOfWater = 100 };
+            Cappuccino cappuccino = new Cappuccino { CoffeeIntensity = Intensity.STRONG, MlOfMilk = 50 };
+            SyrupCappuccino syrupCappuccino = new SyrupCappuccino { CoffeeIntensity = Intensity.LIGHT, MlOfMilk = 40, Syrup = SyrupType.VANILLA };
+            PumpkinSpiceLatte pumpkinSpiceLatte = new PumpkinSpiceLatte { CoffeeIntensity = Intensity.NORMAL, MlOfMilk = 60, MgOfPumpkinSpice = 20 };
+
+            Barista barista = new Barista();
+            barista.PrepareCoffee(americano);
+            barista.PrepareCoffee(cappuccino);
+            barista.PrepareCoffee(syrupCappuccino);
+            barista.PrepareCoffee(pumpkinSpiceLatte);
+        }
+    }
+
